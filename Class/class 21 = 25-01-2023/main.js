bc4225e06e36a3f0be66f361834bd76ae26609fa
zihadulislam99 <zihadulislam778899@gmail.com>
2023-01-26 01:04:32 +0600

    git push

---
+++ Class/class 21 = 25-01-2023/main.js
@@ -0,0 +1,58 @@
+// numeric
+var price = 50;
+var age = 15;
+var temperature = 24;
+var wait = 70;
+var hight = 5.2;
+
+// string
+var name = "Zihadul Talukder";
+var contry = "Bangladesh";
+
+// boolean
+var serious = true;
+var singel = true;
+var marid = false;
+
+// ===========================================================
+
+var egg = 23;
+var onion = 10;
+console.log(egg);
+
+var taka = 10000;
+taka = 100000000000;
+taka += 763;
+
+var a = 100;
+a *= 5;
+console.log(a);
+
+var ab = 16;
+console.log(ab);
+ab++;
+console.log(ab);
+ab--;
+console.log(ab);
+
+// ===========================================================
+
+var fast_Name = "Zihadul"
+var ladt_Name = "Talukder"
+var full_Name = fast_Name + " " + ladt_Name;
+console.log(full_Name);
+
+// ===========================================================
+
+var priceing = "194";
+priceing = parseFloat(priceing)
+console.log(priceing)
+
+var priceing = 150;
+console.log(typeof priceing)
+var priceing = 150.5;
+console.log(typeof priceing)
+var priceing = "Zihadul";
+console.log(typeof priceing)
+var priceing = true;
+console.log(typeof priceing)
