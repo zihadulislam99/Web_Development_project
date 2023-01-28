4f549238a9fb4c6f359234342187d9226defa37b
zihadulislam99 <zihadulislam778899@gmail.com>
2023-01-28 13:39:10 +0600

    git push

---
+++ Class/class 23 = 27-01-2023/main.js
@@ -0,0 +1,79 @@
+var numbers = [42, 54, 68, 66, 53, 99];
+
+var element = numbers[1];
+
+console.log(element);
+
+console.log(numbers);
+numbers[3] = 11;
+console.log(numbers);
+
+var positionIndex = numbers.indexOf(99);
+console.log(positionIndex);
+
+numbers.push(97);
+numbers.push(21);
+numbers.push(45);
+console.log(numbers);
+
+// ========================================================================
+
+var fNames = ["kalam", "salam", "dalam", "malam"];
+console.log(fNames);
+fNames.push("jabbar");
+console.log(fNames);
+fNames.push("kabbar");
+console.log(fNames);
+// ================ ++++++++++++ ----------------- =========================
+fNames.pop()
+console.log(fNames);
+fNames.pop()
+console.log(fNames);
+fNames.pop()
+console.log(fNames);
+
+// ========================================================================
+
+var isGraduated = true;
+var salary = 50000;
+var cars = 0;
+
+
+if(isGraduated == true){
+    console.log(1)
+}
+else{
+    console.log(0)
+}
+
+
+if(isGraduated == true || salary > 50000){
+    console.log(1)
+}
+else{
+    console.log(0)
+}
+
+
+if(isGraduated == true && salary > 50000){
+    console.log(1)
+}
+else{
+    console.log(0)
+}
+
+
+if(isGraduated == true && (salary >= 50000 || cars >= 1)){
+    console.log(1)
+}
+else{
+    console.log(0)
+}
+
+// =================================================================================
+
+var roastGiven = 0;
+while(roastGiven < 7){
+    console.log("Roast Dan, Please!! " + roastGiven)
+    roastGiven++;
+}
