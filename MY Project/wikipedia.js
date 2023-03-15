657b222ece6d8aa2103dd4f0dfe4f98b649ccf46
zihadulislam99 <zihadulislam778899@gmail.com>
2023-03-15 21:17:16 +0600

    full website fenesh

---
+++ MY Project/wikipedia.js
@@ -0,0 +1,7 @@
+const wikipedia = async(searchText) =>{
+    const url = `https://en.wikipedia.org/w/api.php?format=json&action=query&origin=*&prop=extracts&exintro&explaintext&redirects=1&titles=${searchText}`
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data.query.pages);
+}
+// wikipedia("chatGPT");
